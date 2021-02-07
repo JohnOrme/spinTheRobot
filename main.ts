b926615e2597be60bd284d98843acd2c46128ace
JohnOrme <john@orumu.org>
2021-02-07 19:48:50 +0000

    Changed to work with the game Cheeky Monkey board game - https://www.orchardtoys.com/buy/cheeky-monkeys-game_362.htm

--- main.ts
+++ main.ts
@@ -1,27 +1,90 @@
-function randomSpinRight () {
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, Speed)
-    basic.pause(randint(150, MaxTime))
+function RandomSpin (SpinDirection: number) {
+    Result = randint(1, 12)
+    Speed = 100
+    moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
+    moveMotorZIP.setZipLedColor(2, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    moveMotorZIP.setZipLedColor(3, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Yellow))
+    while (Speed > 3) {
+        moveMotorZIP.rotate(1)
+        moveMotorZIP.show()
+        if (SpinDirection < 0) {
+            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, Speed)
+        } else {
+            Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, Speed)
+        }
+        basic.pause(randint(150, MaxTime))
+        Speed += randint(-15, 3)
+    }
+    Kitronik_Move_Motor.stop()
+    if (Result <= 6) {
+        ShowBanana()
+    } else if (Result <= 9) {
+        ShowCheekyMonkey()
+    } else if (Result <= 11) {
+        ShowPool()
+    } else {
+        ShowQuestion()
+    }
+    moveMotorZIP.show()
+}
+function ShowCheekyMonkey () {
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
 }
 input.onButtonPressed(Button.A, function () {
-    Speed = 100
-    while (Speed > 3) {
-        randomSpinRight()
-        Speed += randint(-15, 3)
-    }
-    Kitronik_Move_Motor.stop()
+    music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
+    basic.clearScreen()
+    RandomSpin(1)
 })
-function randomSpinLeft () {
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, Speed)
-    basic.pause(randint(150, MaxTime))
+function ShowPool () {
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
+}
+function ShowBanana () {
+    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Yellow))
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
 }
 input.onButtonPressed(Button.B, function () {
-    Speed = 100
-    while (Speed > 3) {
-        randomSpinLeft()
-        Speed += randint(-15, 3)
-    }
-    Kitronik_Move_Motor.stop()
+    music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
+    basic.clearScreen()
+    RandomSpin(-1)
 })
+function ShowQuestion () {
+    moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . # # .
+        . . . . .
+        . . # . .
+        `)
+}
 let Speed = 0
+let Result = 0
 let MaxTime = 0
-MaxTime = 1000
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
+moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+MaxTime = 500
